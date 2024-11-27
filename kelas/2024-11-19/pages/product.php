2cd6fe3d27e971932acc24bcfb8b19d3416b1be6
Nayla118 <naylaaisyah100@gmail.com>
2024-11-26 20:04:29 -1200

    Add files via upload

---
+++ kelas/2024-11-19/pages/product.php
@@ -0,0 +1,28 @@
+<?php
+$sql = "SELECT * FROM product ORDER BY product ASC";
+echo $sql;
+$hasil = mysqli_query($koneksi, $sql);
+$baris = mysqli_num_rows($hasil);
+if ($baris == 0){
+    echo "<h1>product belum diisi</h1>";
+}
+?>
+
+<div class="product">
+    <h1>product</h1>
+    <?php
+    if ($baris > 0){
+        while ($row = mysqli_fetch_assoc($hasil));
+    ?>
+    <div class="detail-product">
+        <h2><?= $row["product"]?></h2>
+        <img src="images/<?= $row["gambar"] ?>" alt="">
+        <p><?= $row["deskripsi"]?></p>
+        <p><?= $row["stock"]?></p>
+        <p><strong><?= $row["harga"]?></strong></p>
+        <a href="?menu=cart&add=<?= $row ["id"]?>"><button>BELI</button></a>
+    </div>
+    <?php
+    }
+    ?>
+</div>
